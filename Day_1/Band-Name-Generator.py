dcd9acb2b40d5cc631005a87bc45d3b38c12db3e
Archie <110494298+archie122@users.noreply.github.com>
2023-07-07 09:55:10 -0600

    Day 1
    The code was pretty simple and a very good starting point

---
+++ Day_1/Band-Name-Generator.py
@@ -0,0 +1,6 @@
+print("Hello!! This is Simple Program that Generators a Band Name for you!!")
+
+cityName = input("Enter the city that you grew up in: \n")
+petName = input("Enter your pet's name: \n")
+
+print("Your band name is : " + cityName + " " + petName)
